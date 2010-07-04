557158d15c190d898e9a38f4f47ba579a518f50c
Conor Horan-Kates <conor.code@gmail.com>
2010-07-03 18:54:45 -0600

    gfight - initial, stub commit

---
+++ gfight/gfight.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl -w
+#  gfight.pl --
+
+use strict;
+use warnings;
+use 5.010;
+
+use lib '/home/conor/Dropbox/perl/_pm';
+use lib 'c:/_dropbox/My Dropbox/perl/_pm';
+use webroot;
+
+#(02:02:27 PM) Conor Horan-Kates: Write a google fight script -- compare the number of results for two different strings
+#(02:02:42 PM) Conor Horan-Kates: Save past queries in a YAML file
+
+
+exit 0;
+
+## subs below
