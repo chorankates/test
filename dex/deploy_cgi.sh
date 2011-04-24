cfad4a1d541dc164bff21af78e1f07164ed6ce1d
Conor Horan-Kates <conor.code@gmail.com>
2011-04-24 14:54:04 -0700

    dex - like i said

--- dex/deploy_cgi.sh
+++ dex/deploy_cgi.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+run_dir=/home/conor/dex/
+
+# copy the wrapper into apache
 cp -v dex.cgi /usr/local/apache2/cgi-bin/
 chmod -v +x /usr/local/apache2/cgi-bin/dex.cgi
+
+# copy dex-crawl.pl into the running dir
+cp -v dex-crawl.pl $run_dir
+
+# copy the latest db into the running dir
+cp -v dex.sqlite $run_dir
