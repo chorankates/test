cbeb22af42712e7e19513800edc56387aa64c890
Conor Horan-Kates <conor.code@gmail.com>
2011-04-25 18:17:05 -0700

    dex - updated deploy script to include deploying library

--- dex/deploy_cgi.sh
+++ dex/deploy_cgi.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 run_dir=/home/conor/dex/
+lib_dir=/home/conor/dex/lib/dex/
 
 # copy the wrapper into apache
 cp -v dex.cgi /usr/local/apache2/cgi-bin/
@@ -8,6 +9,9 @@ chmod -v +x /usr/local/apache2/cgi-bin/dex.cgi
 
 # copy dex-crawl.pl into the running dir
 cp -v dex-crawl.pl $run_dir
+
+# copy util.pm
+cp -v lib/dex/util.pm $lib_dir
 
 # copy the latest db into the running dir
 cp -v dex.sqlite $run_dir
